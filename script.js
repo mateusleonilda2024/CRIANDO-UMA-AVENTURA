9ef61888399e1a35a400e7777dd0f75194899eb7
chima <schmoller.mateus@escola.pr.gov.br>
2024-12-03 13:41:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-avanc.forEach (button => {
+avanca.forEach (button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
